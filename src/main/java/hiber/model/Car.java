10b2aa08d19b00d6688df4489427469ff04c18c6
DaniilNov <xa86@mail.ru>
2020-01-10 17:33:09 +0300

    Initial commit

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,45 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue
+    private Long id;
+    String name;
+    int series;
+
+
+    public Car() {
+    }
+
+    public Car(String name, int series) {
+        this.name = name;
+        this.series = series;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+}
